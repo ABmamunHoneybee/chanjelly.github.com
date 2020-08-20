ac16093e7fdb2d6e9ebc2b6357f1b613a9a9268e
ABmamunHoneybee <69682640+ABmamunHoneybee@users.noreply.github.com>
2020-08-20 21:28:23 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,15 @@ $.ajax({
       $("footer").html(data)
     }
 });
+$(".chng_nav").click(function(){
+ $(".chng_nav").removeClass("chng_nav_active");
+ $(this).addClass("chng_nav_active");
+ var url = $(this).data("url");
+ $.ajax({
+  url: url,
+  type: 'GET',
+  success: function(data) {        
+    $(".crd").html(data)
+  }
+});
+});
